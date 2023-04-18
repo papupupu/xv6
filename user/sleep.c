e492826047fa8d2fbfeb56865c5c513a290fce31
papupupu <2910935654@qq.com>
2023-04-18 16:28:53 +0800

    sleep.c_1.0

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc <= 1){
+        printf("please enter the time\n");
+        exit(0);
+    }
+    printf("\n");
+    int time = atoi(argv[1]);
+    if(time < 0){
+       printf("enter time error\n");
+    }
+    sleep(time);
+    exit(0);
+}
